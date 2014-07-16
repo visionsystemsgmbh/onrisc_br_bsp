c83dc6730ba2fc8cc47792a644587ac71f7a93b2
Vision Systems GmbH <contact@visionsystems.de>
2014-07-16 14:58:14 +0200

    libonrisc: bump to 1.1.0

--- package/libonrisc/libonrisc.mk
+++ package/libonrisc/libonrisc.mk
@@ -3,7 +3,7 @@
 # libonrisc
 #
 #############################################################
-LIBONRISC_VERSION = master
+LIBONRISC_VERSION = 1.1.0
 LIBONRISC_SITE = git://git.vscom.de/libonrisc
 LIBONRISC_DEPENDENCIES = libsoc
 LIBONRISC_INSTALL_STAGING = YES
