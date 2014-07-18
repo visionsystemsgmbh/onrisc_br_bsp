d1ff5138b7a92bea4dffbd97d1a7fb3f16b6b57e
Vision Systems GmbH <contact@visionsystems.de>
2014-07-18 12:49:51 +0200

    Balios:FIT: create standard kernel-fit.itb image

--- board/vscom/balios/custom-balios_ir5221-FIT.sh
+++ board/vscom/balios/custom-balios_ir5221-FIT.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cp $2/board/vscom/balios/kernel-fit-balios-ir5221.its $1/
-mkimage -f $1/kernel-fit-balios-ir5221.its $1/kernel-fit-balios-ir5221.itb
+mkimage -f $1/kernel-fit-balios-ir5221.its $1/kernel-fit.itb
 
 exit 0
