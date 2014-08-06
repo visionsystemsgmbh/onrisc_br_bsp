80330c25c948ba46ac337a1e9384360da856f154
Vision Systems GmbH <contact@visionsystems.de>
2014-08-06 12:26:55 +0200

    libonrisc: add libudev dependency

--- package/libonrisc/libonrisc.mk
+++ package/libonrisc/libonrisc.mk
@@ -5,7 +5,7 @@
 #############################################################
 LIBONRISC_VERSION = 1.1.0
 LIBONRISC_SITE = $(call github,yegorich,libonrisc,$(LIBONRISC_VERSION))
-LIBONRISC_DEPENDENCIES = libsoc
+LIBONRISC_DEPENDENCIES = libsoc eudev
 LIBONRISC_INSTALL_STAGING = YES
 
 $(eval $(cmake-package))
