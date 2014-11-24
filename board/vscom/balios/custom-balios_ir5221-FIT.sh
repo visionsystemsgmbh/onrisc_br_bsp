a7336ded8a5cc13799a96ef9003276d341cac5cc
Vision Systems GmbH <contact@visionsystems.de>
2014-11-24 14:58:51 +0100

    Baltos: rework DTS support
    Move all DTS files to board/vscom/dts/ folder on kernel version
    basis.

--- board/vscom/balios/custom-balios_ir5221-FIT.sh
+++ board/vscom/balios/custom-balios_ir5221-FIT.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp $2/board/vscom/balios/kernel-fit-balios-ir5221.its $1/
-mkimage -f $1/kernel-fit-balios-ir5221.its $1/kernel-fit.itb
+cp $2/board/vscom/balios/kernel-fit.its $1/
+mkimage -f $1/kernel-fit.its $1/kernel-fit.itb
 
 exit 0
