7402c798250f0b219eeb56df7641f0a949503b2e
Vision Systems GmbH <contact@visionsystems.de>
2015-02-26 17:37:50 +0100

    libonrisc: enable Python wrapper

--- package/libonrisc/libonrisc.mk
+++ package/libonrisc/libonrisc.mk
@@ -8,5 +8,11 @@ LIBONRISC_SITE = $(call github,visionsystemsgmbh,libonrisc,$(LIBONRISC_VERSION))
 LIBONRISC_DEPENDENCIES = libsoc eudev
 LIBONRISC_INSTALL_STAGING = YES
 
+ifeq ($(BR2_PACKAGE_LIBONRISC_PYTHON_WRAPPER),y)
+	LIBONRISC_CONF_OPTS += -DPYTHON_WRAP=ON
+else
+	LIBONRISC_CONF_OPTS += -DPYTHON_WRAP=OFF
+endif
+
 $(eval $(cmake-package))
 
