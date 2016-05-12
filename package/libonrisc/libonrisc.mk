63ba698e6ca14ccdb8b17a02528aabe90ef7c0c2
Vision Systems GmbH <contact@visionsystems.de>
2016-05-12 09:09:14 +0200

    libonrisc: bump to 1.5.2

--- package/libonrisc/libonrisc.mk
+++ package/libonrisc/libonrisc.mk
@@ -3,9 +3,9 @@
 # libonrisc
 #
 #############################################################
-LIBONRISC_VERSION = 1.5.1
+LIBONRISC_VERSION = 1.5.2
 LIBONRISC_SITE = $(call github,visionsystemsgmbh,libonrisc,$(LIBONRISC_VERSION))
-LIBONRISC_DEPENDENCIES = libsoc eudev
+LIBONRISC_DEPENDENCIES = libsoc eudev host-pkgconf
 LIBONRISC_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PACKAGE_LIBONRISC_PYTHON_WRAPPER),y)
@@ -16,4 +16,3 @@ else
 endif
 
 $(eval $(cmake-package))
-
