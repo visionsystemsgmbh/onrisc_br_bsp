46443d65e7361207aa39623bf5b82c0200a78fc2
Vision Systems GmbH <contact@visionsystems.de>
2016-05-13 10:48:18 +0200

    Baltos: add genimage support
    genimage-baltos.cfg provides configuration for an SD card image
    containing two partitions: FAT (boot) and ext4 (rootfs). After
    post-image.sh execution images folder will have sdcard.img, that
    can be burned onto SD card directly.

---
+++ board/vscom/baltos/post-image.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+BOARD_DIR="$(dirname $0)"
+BOARD_NAME="$(basename ${BOARD_DIR})"
+GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+rm -rf "${GENIMAGE_TMP}"
+
+cp $2/board/vscom/baltos/uEnv.txt $1/
+
+genimage                           \
+	--rootpath "${TARGET_DIR}"     \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
+
+exit $?
