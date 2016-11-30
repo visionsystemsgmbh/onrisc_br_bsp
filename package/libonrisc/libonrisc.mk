0e13a0d1aecafef9e2f1cfe14c59f0137080f4a7
Vision Systems GmbH <contact@visionsystems.de>
2016-11-30 10:53:31 +0100

    libonrisc: bump to version 1.5.3
    Make Python wrapper depend on whether Python is selected
    in Buildroot.

--- package/libonrisc/libonrisc.mk
+++ package/libonrisc/libonrisc.mk
@@ -3,12 +3,12 @@
 # libonrisc
 #
 #############################################################
-LIBONRISC_VERSION = 1.5.2
+LIBONRISC_VERSION = 1.5.3
 LIBONRISC_SITE = $(call github,visionsystemsgmbh,libonrisc,$(LIBONRISC_VERSION))
 LIBONRISC_DEPENDENCIES = libsoc eudev host-pkgconf
 LIBONRISC_INSTALL_STAGING = YES
 
-ifeq ($(BR2_PACKAGE_LIBONRISC_PYTHON_WRAPPER),y)
+ifeq ($(BR2_PACKAGE_PYTHON)$(BR2_PACKAGE_PYTHON3),y)
 	LIBONRISC_DEPENDENCIES += host-swig
 	LIBONRISC_CONF_OPTS += -DPYTHON_WRAP=ON -DSWIG_EXECUTABLE=$(SWIG)
 else
