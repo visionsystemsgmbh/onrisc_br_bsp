3c34c0724d8f57c6cd9160e0a44d453006276c5e
Vision Systems GmbH <contact@visionsystems.de>
2017-03-21 14:18:17 +0100

    FIT: make script more portable
    With these changes the script can now be run both as BR2_ROOTFS_POST_BUILD_SCRIPT
    and BR2_ROOTFS_POST_IMAGE_SCRIPT.

--- board/vscom/baltos/custom-FIT.sh
+++ board/vscom/baltos/custom-FIT.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp $2/board/vscom/baltos/kernel-fit.its $1/
-mkimage -f $1/kernel-fit.its $1/kernel-fit.itb
+cp $2/board/vscom/baltos/kernel-fit.its "${BINARIES_DIR}/"
+mkimage -f "${BINARIES_DIR}/kernel-fit.its" "${BINARIES_DIR}/kernel-fit.itb"
 
 exit 0
