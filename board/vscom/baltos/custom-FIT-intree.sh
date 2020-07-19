431024fa7745a58b89d937a1a6c0ce47d8659b02
Vision Systems GmbH <contact@visionsystems.de>
2020-07-19 13:46:18 +0200

    FIT script: check return value of the mkimage command

--- board/vscom/baltos/custom-FIT-intree.sh
+++ board/vscom/baltos/custom-FIT-intree.sh
@@ -2,5 +2,10 @@
 
 cp $2/board/vscom/baltos/kernel-fit-intree.its $1/
 mkimage -f $1/kernel-fit-intree.its $1/kernel-fit.itb
+rc=$?
+if [ $rc != 0 ]; then
+        echo Failed to create kernel-fit.itb
+        exit 1
+fi
 
 exit 0
